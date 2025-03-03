0e65ca3c2008c5290452ff9c721121723ecb747b
DMPeek <dustinmpeek@outlook.com>
2025-03-03 10:20:34 -0600

    adding content

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("button1").addEventListener("click", function() {
+    alert("I do be needing to work on some CSS")
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+    alert("I do indeed need to work on some efficiency")
+});
+
+document.getElementById("button3").addEventListener("click", function() {
+    alert("I do a lil practice for my memory")
+});
